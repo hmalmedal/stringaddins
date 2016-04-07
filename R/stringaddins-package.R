52b04dc6ff46f54465361942eb76ed9692ea2cf0
Håkon Malmedal <hmalmedal@gmail.com>
2016-04-07 10:35:50 +0200

    Initial commit

---
+++ R/stringaddins-package.R
@@ -0,0 +1,40 @@
+#' Manipulate Strings
+#'
+#' RStudio add-ins for manipulating strings.
+#'
+#' Open a file in RStudio. Select text and execute one of the add-ins.
+#'
+#' @name stringaddins
+#' @docType package
+NULL
+
+change_region <- function(f) {
+  context <- tryCatch(rstudioapi::getActiveDocumentContext(),
+                      error = function(e) NULL)
+  if (!is.null(context)) {
+    lapply(context$selection, function(sel) {
+      rstudioapi::modifyRange(sel$range, f(sel$text))
+    })
+  }
+  invisible()
+}
+
+capitalize_region <- function() {
+  change_region(stringi::stri_trans_totitle)
+}
+
+downcase_region <- function() {
+  change_region(stringi::stri_trans_tolower)
+}
+
+upcase_region <- function() {
+  change_region(stringi::stri_trans_toupper)
+}
+
+escape_region <- function() {
+  change_region(stringi::stri_escape_unicode)
+}
+
+unescape_region <- function() {
+  change_region(stringi::stri_unescape_unicode)
+}
